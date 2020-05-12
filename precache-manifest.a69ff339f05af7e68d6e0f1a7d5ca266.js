c520640ed6d35fcd5543c96f8c3975485204aba9
stempest <stempest@163.com>
2020-05-12 19:00:05 +0800

    first commit

---
+++ precache-manifest.a69ff339f05af7e68d6e0f1a7d5ca266.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f17f3546312ef32f544f27fd352d1d90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e214bb7b04ba01f998c",
+    "url": "/static/css/main.3eafcb2e.chunk.css"
+  },
+  {
+    "revision": "3786750c1d5d878327d5",
+    "url": "/static/js/2.2cc84e4a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2cc84e4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e214bb7b04ba01f998c",
+    "url": "/static/js/main.7f0a1520.chunk.js"
+  },
+  {
+    "revision": "d0a598c2c4f910599d51",
+    "url": "/static/js/runtime-main.e7310a31.js"
+  },
+  {
+    "revision": "15974853bc3294ef68e7e6d58fe74fd7",
+    "url": "/static/media/PingFang-Medium.15974853.ttf"
+  },
+  {
+    "revision": "3e78e808a274ec2caf5dce50012d62ad",
+    "url": "/static/media/banner.3e78e808.mp4"
+  },
+  {
+    "revision": "47f71d48d19bef20e76c5ada47bbed04",
+    "url": "/static/media/banner.47f71d48.jpg"
+  },
+  {
+    "revision": "d86373163381b93026952492152ba30d",
+    "url": "/static/media/music.d8637316.mp3"
+  },
+  {
+    "revision": "06ec9738b30f8a9c259e66bedde9c1a5",
+    "url": "/static/media/wind.06ec9738.mp3"
+  }
+]);
